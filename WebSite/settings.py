405697db693ca9bea0bb904e87d9a7322bc2ed23
İbrahim Ediz <Kurs>
2019-12-22 14:31:50 +0200

    22122019_4

--- WebSite/settings.py
+++ WebSite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Blog',
     'KayitAl',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -87,18 +88,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+   
 ]
 
 
