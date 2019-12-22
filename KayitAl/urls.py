f381a4b7301dd65788283170df11cf72509aaeac
İbrahim Ediz <Kurs>
2019-12-22 15:00:57 +0200

    22122019_3

--- KayitAl/urls.py
+++ KayitAl/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.kayitlist, name='kayit_list'),
+    path('kayit/liste', views.kayitlist, name='kayit_list'),
     path('kayit/<int:pk>/',views.kayitdetay,name="kayitagit"),
     path('kayit/yeni',views.yenikayit,name="kayityeni"),
     path('kayit/duzenle/<int:pk>',views.kayit_duzenle,name="kayitduzenle"),
